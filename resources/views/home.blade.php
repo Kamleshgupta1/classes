a55d808337f0436864aabf97e50d480793d361af
Kamlesh Gupta <kamleshguptapm@gmail.com>
2019-03-23 20:07:04 +0530

    Add existing file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     
     <div class="col-8">
         <div class="row">
-            <div class="col-6">
+            <div class="col-10 mb-2">
                   <div class="card">
                     <img class="card-img-top" src=".../100px180/" alt="Card image cap">
                     <div class="card-body">
@@ -18,18 +18,8 @@
                       <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                   </div>
-                  <div class="card">
-                    <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                  </div>
             </div>
-            <div class="col-6">
+            <div class="col-10">
                   <div class="card">
                     <img class="card-img-top" src=".../100px180/" alt="Card image cap">
                     <div class="card-body">
@@ -40,17 +30,8 @@
                       <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                   </div>
-                  <div class="card">
-                    <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                  </div>      
             </div>
+
         </div>
     </div>
     
