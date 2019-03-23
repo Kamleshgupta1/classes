fba298ad4df14a8b0f0e4380ee820c2909fa7e2b
Kamlesh Gupta <kamleshguptapm@gmail.com>
2019-03-23 18:05:12 +0530

    Add existing file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -229,7 +229,7 @@
 <footer class="py-sm-5 py-4">
 	<div class="container py-md-3">
 		<div class="footer-grids text-center" style="color:#fff;">
-			<a href="#!" style="color:#fff;"><h3><img class="logos" src="/storage/cover_images/noimage.jpg"> Electrolux Sales and Service Centre</h3></a>
+			<a href="#!" style="color:#fff;"><h3><img class="logos" src="/storage/cover_images/noimage.jpg"> Pace Tutorial</h3></a>
 		</div>
 		<!-- to top -->
 		<div class="top-icon text-center mt-2">
@@ -239,28 +239,27 @@
 		<div class="middle mt-3">
 			<ul class="social mb-4">
 				<li><a href="#"><span class="fa fa-facebook icon_facebook"></span></a></li>
+                <li><a href="#"><span class="fa fa-instagram"></span></a></li>
 				<li><a href="#"><span class="fa fa-twitter icon_twitter"></span></a></li>
+                <li><a href="#"><span class="fa fa-whatsapp"></span></a></li>
+                <li><a href="#"><span class="fa fa-linkedin icon_linkedin"></span></a></li>
+                <li><a href="#"><span class="fa fa-envelope-square"></span></a></li>
 				<li><a href="#"><span class="fa fa-google-plus icon_google-plus"></span></a></li>
-				<li><a href="#"><span class="fa fa-linkedin icon_linkedin"></span></a></li>
 			</ul>
 		</div>
-        <div class="row text-center">
-            <div class="col-4">
-                <span class="copyright">Copyright &copy; This Website 2019</span>
-            </div>  
-            <div class="col-4">
+        <div class="text-center">
+            <span class="copyright">Copyright &copy; This Website 2019</span>
+        </div>  
+        <div class="intro text-center">
                 <h5>Hi, this is Kamlesh Gupta. Contact me for your application.</h5>
-            </div>
-            <div class="middle col-4">
+        </div>
+        <div class="middle text-center">
 			<ul class="social mb-4">
-				<li><a href="#"><span class="fa fa-facebook icon_facebook"></span></a></li>
-				<li><a href="#"><span class="fa fa-twitter icon_twitter"></span></a></li>
-				<li><a href="#"><span class="fa fa-google-plus icon_google-plus"></span></a></li>
-				<li><a href="#"><span class="fa fa-linkedin icon_linkedin"></span></a></li>
+                <li><a href="#"><span class="fa fa-linkedin icon_linkedin"></span></a></li>
+				<li><a href="#"><span class="fa fa-envelope-square"></span></a></li>
 			</ul>
 		</div>
         </div>
-	</div>
 </footer>
         
 </body>
