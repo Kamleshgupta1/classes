2cacb04a376a8d99511c7466eb7a1f02785b7786
Kamlesh Gupta <kamleshguptapm@gmail.com>
2019-03-23 13:47:14 +0530

    Add existing file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -203,5 +203,28 @@
 	</div>
 </section>
         
-    </body>
+<!-- footer -->
+<footer class="py-sm-5 py-4">
+	<div class="container py-md-3">
+		<div class="footer-grids text-center" style="color:#fff;">
+			<a href="#!" style="color:#fff;"><h3><img class="logos" src="/storage/cover_images/noimage.jpg"> Electrolux Sales and Service Centre</h3></a>
+		</div>
+		<!-- to top -->
+		<div class="top-icon text-center mt-2">
+			<a href="#" class="move-top text-center"><span class="fa fa-angle-double-up" aria-hidden="true"></span> Go to top</a>
+		</div>
+		<!-- //to top -->
+		<div class="middle mt-3">
+			<ul class="social mb-4">
+				<li><a href="#"><span class="fa fa-facebook icon_facebook"></span></a></li>
+				<li><a href="#"><span class="fa fa-twitter icon_twitter"></span></a></li>
+				<li><a href="#"><span class="fa fa-google-plus icon_google-plus"></span></a></li>
+				<li><a href="#"><span class="fa fa-linkedin icon_linkedin"></span></a></li>
+				<li><a href="#"><span class="fa fa-dribbble icon_dribbble"></span></a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+        
+</body>
 </html>
